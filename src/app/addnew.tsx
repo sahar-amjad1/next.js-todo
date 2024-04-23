4be1a9ed4137dfdf605d4e0e0a71f432f919a6a0
sahar-amjad1 <150724877+sahar-amjad1@users.noreply.github.com>
2024-04-23 17:18:03 +0500

    todo comp

---
+++ src/app/addnew.tsx
@@ -0,0 +1,8 @@
+export default function AddNewTodo(){
+    return(
+        <div>
+            <input type="text"/>
+            <button className="w-full max-w-sm bg-slate-800 text-white">Add</button>
+        </div>
+    );
+}
---
+++ src/app/todolist.tsx
@@ -0,0 +1,14 @@
+export default function TodoList(){
+    return(
+        <div>
+          <ul style={{ lifeStyleType: "none" , padding:0}}> 
+          <li style={{ padding: "5px 0"}} > Learn Next.js</li>
+          <li style={ {padding : "5px 0"}}> Learn Paython</li>
+          <li style={{ padding: "5px 0"}} >Start New sideProject</li>
+          
+          </ul>
+        </div>
+        
+      
+    )
+}
